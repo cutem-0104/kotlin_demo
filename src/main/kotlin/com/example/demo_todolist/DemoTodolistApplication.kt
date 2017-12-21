7aa28ff048a8a4ccbcf48e56e0c6ac37ba4b51b8
shuhei.sugaya <shuhei.sugaya>
2017-12-21 23:58:01 +0900

    h2DB導入

--- src/main/kotlin/com/example/demo_todolist/DemoTodolistApplication.kt
+++ src/main/kotlin/com/example/demo_todolist/DemoTodolistApplication.kt
@@ -1,10 +1,22 @@
 package com.example.demo_todolist
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-class DemoTodolistApplication
+class DemoTodolistApplication {
+    @Bean
+    fun commandLineRunner(jdbcTemplate: JdbcTemplate) = CommandLineRunner {
+        jdbcTemplate.execute("""CREATE TABLE IF NOT EXISTS task (
+            id      BIGINT       PRIMARY KEY AUTO_INCREMENT,
+            content VARCHAR(100) NOT NULL,
+            done    BOOLEAN      NOT NULL    DEFAULT FALSE
+        )""")
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<DemoTodolistApplication>(*args)
